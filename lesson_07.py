0831cd89ce726a896733e006d56479430d46f7ce
Joshua VanKirk <vankirk.josh@gmail.com>
2013-07-18 09:36:25 -0400

    Added the study drills.

--- lesson_07.py
+++ lesson_07.py
@@ -1,9 +1,9 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." * 10 #multiplied the . by 10.....I think.
+print "Mary had a little lamb."		# prints the line
+print "Its fleece was white as %s." % 'snow'     # prints the line and subs in %s with snow
+print "And everywhere that Mary went."  # basic print
+print "." * 10 #multiplied the . by 10
 
-end1 = "C"
+end1 = "C"  # these are just adding specific letters to variables to be printed later.
 end2 = "h"
 end3 = "e"
 end3 = "e"
@@ -18,5 +18,5 @@ end11 = "e"
 end12 = "r"
 
 
-print end1 + end2 + end3 + end4 + end5 + end6, // comma makes it stay on 2 lines...
-print end7 + end8 + end9 + end10 + end11 + end12
+print end1 + end2 + end3 + end4 + end5 + end6, # comma makes it stay on 2 lines...
+print end7 + end8 + end9 + end10 + end11 + end12 # This is the variable from earlier...the are just printed out in a line to spell cheese burger
